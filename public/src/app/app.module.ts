946fe541f055bd7e4319c2ec50fe24037e9158d5
Wilson Ramstead <ramsteadwilson@gmail.com>
2019-11-25 16:48:57 -0800

    deleted all original components, base update is now complete. Cleaned up entire project of sooo much code.

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,18 +6,8 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeadComponent } from './main-head/main-head.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainInfoComponent } from './main-info/main-info.component';
-import { PortHeadComponent } from './port-head/port-head.component';
-import { ProjectOneComponent } from './project-one/project-one.component';
-import { ContactComponent } from './contact/contact.component';
-import { TechComponent } from './tech/tech.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProjectTwoComponent } from './project-two/project-two.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { ProjectThreeComponent } from './project-three/project-three.component';
-import { ProjectFourComponent } from './project-four/project-four.component';
 import { MainBodyComponent } from './main-body/main-body.component';
 import { AboutComponent } from './main-body/about/about.component';
 import { WhatIDoComponent } from './main-body/what-ido/what-ido.component';
@@ -32,17 +22,7 @@ import { ThisWebsiteComponent } from './main-body/this-website/this-website.comp
   declarations: [
     AppComponent,
     MainHeadComponent,
-    AboutMeComponent,
     FooterComponent,
-    MainInfoComponent,
-    PortHeadComponent,
-    ProjectOneComponent,
-    ContactComponent,
-    TechComponent,
-    NavbarComponent,
-    ProjectTwoComponent,
-    ProjectThreeComponent,
-    ProjectFourComponent,
     MainBodyComponent,
     AboutComponent,
     WhatIDoComponent,
